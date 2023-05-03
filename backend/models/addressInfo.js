87ba29c4c0d33fab55d9e843428c7c7be432b0f5
hogwon <hokwonlee88@gmail.com>
2023-05-03 13:08:13 +0900

    id ==> address_id DB 컬럼명이랑 동일하게 수정

--- backend/models/addressInfo.js
+++ backend/models/addressInfo.js
@@ -1,70 +1,70 @@
-'use strict'
+'use strict';
 
-const { Sequelize, DataTypes } = require('sequelize')
+const {Sequelize, DataTypes} = require('sequelize');
 
 module.exports = sequelize => {
-
-  const model = sequelize.define('Address', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
+  const model = sequelize.define(
+    'Address',
+    {
+      address_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      is_default: {
+        type: DataTypes.TINYINT,
+        allowNull: true,
+      },
+      post_code: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address1: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address2: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address_tag: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address3: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address_active: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      recipient: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      recipient_phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      user_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    is_default: {
-      type: DataTypes.TINYINT,
-      allowNull: true,
+    {
+      tableName: 'addressinfo',
+      timestamps: false,
     },
-    post_code: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address1: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address2: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address_tag: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address3: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address_active: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    recipient: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    recipient_phone: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    user_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-  }, {
-    tableName: 'addressinfo',
-    timestamps: false
-  })
+  );
 
   model.findAllByUserId = async userId => {
     return await model.findAll({
       where: {
         user_id: userId,
-      }
-    })
-  }
+      },
+    });
+  };
 
-
-  return model
-
-}
+  return model;
+};
