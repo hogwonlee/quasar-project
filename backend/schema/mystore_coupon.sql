ec192db4ef76211d1239067de90e5c01e13a2c00
hogwon <hokwonlee88@gmail.com>
2023-08-10 19:56:51 +0900

    쿠폰 추가 스키마 위치 수정

--- backend/schema/mystore_coupon.sql
+++ backend/schema/mystore_coupon.sql
@@ -10,3 +10,20 @@ CREATE TABLE `coupon` (
   `limit_date` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+
+INSERT INTO `mystore`.`coupon`
+(`id`,
+`coupon_name`,
+`coupon_name_ko`,
+`gift_condition`,
+`coupon_price`,
+`use_condition`,
+`limit_date`)
+VALUES
+(
+1, '开业优惠券', '오픈 기념 쿠폰',0, 3000, 30000, 90
+),
+(2, '5353优惠券', '5353할인 쿠폰', 50000, 3000, 50000, 90
+),
+(3, '感恩优惠券', '감사 쿠폰', NULL, 3000, NULL, NULL
+);
