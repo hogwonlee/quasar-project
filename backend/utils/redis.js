1d0d811582526ce4aae4239b765bf029c354e007
yihoyoung <yihoyoung@nate.com>
2023-08-11 01:03:06 +0900

    레디스 세션 시간 연장

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -1,20 +1,20 @@
+const Redis = require('ioredis');
 
-const { createClient } = require('redis');
-
-const client = createClient({
-  url: 'redis:localhost:6379'
-});
+const redis = new Redis();
 
 const thirtyMinuteOnSecond = 1800;
-
-
 module.exports = {
   // 세션을 30분 연장하고 유저정보 입력
   setToken: async (token, user) => {
-    return await client.setEx(token, thirtyMinuteOnSecond, user)
+    return await redis.setex(token, thirtyMinuteOnSecond, JSON.stringify(user));
   },
   // 세션을 찾고 있으면 30분 연장
-  getToken: async (token) => {
-    return await client.getEx(token, thirtyMinuteOnSecond)
-  }
-}
+  getToken: async token => {
+    if (!token) return null;
+    let sUser = await redis.get(token);
+    if (!sUser) {
+      await redis.setex(token, thirtyMinuteOnSecond, sUser);
+    }
+    return JSON.parse(sUser);
+  },
+};
