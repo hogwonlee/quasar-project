e72c1ebac538062aa49276c560d28f02ac92e7b2
yihoyoung <yihoyoung@nate.com>
2023-09-03 18:34:52 +0900

    update toss key place

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,5 +1,11 @@
+import jsonFile from './index.json';
+
+
+process.env.VUE_APP_BACKEND_SERVER = jsonFile.VUE_APP_BACKEND_SERVER;
+process.env.VUE_APP_TOSS_CLIENT_KEY = jsonFile.VUE_APP_TOSS_CLIENT_KEY;
+process.env.VUE_APP_TOSS_SECRET_KEY = jsonFile.VUE_APP_TOSS_SECRET_KEY;
 export default {
-  server: process.env.BACKEND_SERVER || 'http://175.119.224.213:3000', //'http://182.215.249.216:3000', // 'http://localhost:3000',
-  clientKey: process.env.TOSS_CLIENT_KEY,
-  secretKey: process.env.TOSS_SECRET_KEY,
+  server: process.env.VUE_APP_BACKEND_SERVER || 'http://175.119.224.213:3000', //'http://182.215.249.216:3000', // 'http://localhost:3000',
+  clientKey: process.env.VUE_APP_TOSS_CLIENT_KEY,
+  secretKey: process.env.VUE_APP_TOSS_SECRET_KEY,
 };
