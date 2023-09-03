0a08a75a3f823d4186c2a0d2a4fa4c0e9f719975
yihoyoung <yihoyoung@nate.com>
2023-09-03 23:14:06 +0900

    add long time token

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -3,6 +3,7 @@ const Redis = require('ioredis');
 const redis = new Redis();
 
 const thirtyMinuteOnSecond = 1800;
+const thiryMonthOnSecond = 2592000;
 module.exports = {
   // 세션을 30분 연장하고 유저정보 입력
   setToken: async (token, user) => {
@@ -15,6 +16,24 @@ module.exports = {
     if (!sUser) return null;
     if (sUser) {
       await redis.setex(token, thirtyMinuteOnSecond, sUser);
+    }
+
+    try {
+      return JSON.parse(sUser);
+    } catch (error) {
+      console.error(error)
+      return null
+    }
+  },
+  setLongToken: async (token, user) => {
+    return await redis.setex(token, thiryMonthOnSecond, JSON.stringify(user));
+  },
+  getLongToken: async token => {
+    if (!token) return null;
+    let sUser = await redis.get(token);
+    if (!sUser) return null;
+    if (sUser) {
+      await redis.setex(token, thiryMonthOnSecond, sUser);
     }
 
     try {
