d1910308376b169df11ed487742bd63b12e9de12
hogwon <hokwonlee88@gmail.com>
2023-09-25 22:27:36 +0900

    jwt secret 키값 server_2.js 파일과 동일하게 수정

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -27,7 +27,7 @@ function hashpw(password) {
 }
 
 let jwtObj = {
-  secret: 'mysecrettoken', // 원하는 시크릿 키
+  secret: 'cfomarketHogwon032^secret', // 원하는 시크릿 키
   option: {
     algorithm: 'HS256', // 해싱 알고리즘
     expiresIn: '300m', // 토큰 유효 기간 (테스트 기간 5시간으로 변경)
@@ -38,18 +38,18 @@ let jwtObj = {
 module.exports = {
   checkAuth: async (req, res, next) => {
     if (req.headers.authorization == null) {
-      res.status(401).send({ msg: 'error', content: 'no authrozation' });
+      res.status(401).send({msg: 'error', content: 'no authrozation'});
       return;
     }
     let userInfo = await redisController.getToken(req.headers.authorization);
     if (!userInfo) {
-      res.status(401).send({ msg: 'error', content: 'session time out.' });
+      res.status(401).send({msg: 'error', content: 'session time out.'});
       return;
     }
 
     jwt.verify(req.headers.authorization, jwtObj.secret, (err, decoded) => {
       if (err) {
-        res.status(401).send({ msg: 'error', content: err });
+        res.status(401).send({msg: 'error', content: err});
         return;
       }
 
@@ -72,7 +72,7 @@ module.exports = {
     if (req.session.user) {
       // 세션에 유저가 존재한다면
       console.log('이미 로그인 돼있습니다~');
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' });
+      res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
       res.write('<h1> already Login</h1>');
       res.end();
     } else {
@@ -82,11 +82,11 @@ module.exports = {
         (err, results, fields) => {
           if (err) {
             console.error(err);
-            res.status(500).send({ msg: 'error', content: err });
+            res.status(500).send({msg: 'error', content: err});
           }
           if (results.length <= 0) {
             console.log('로그인요청:' + err);
-            res.status(400).send({ msg: 'error', content: err });
+            res.status(400).send({msg: 'error', content: err});
           } else {
             console.log(JSON.stringify(results));
             req.session.cookie.user = {
@@ -103,7 +103,7 @@ module.exports = {
               jwtObj.option,
             );
             redisController.setToken(token, req.session.cookie.user);
-            res.status(200).send({ token, results });
+            res.status(200).send({token, results});
             // });
           }
         },
@@ -125,7 +125,7 @@ module.exports = {
     db.query(sqlCommend, param, (err, results, fields) => {
       if (err) {
         console.log('회원가입요청:' + err);
-        res.status(400).send({ msg: 'error', content: err });
+        res.status(400).send({msg: 'error', content: err});
       } else {
         //오픈 이벤트: 특정 날짜 이전에 계정 생성 시, 쿠폰 지급
         if (Date.now() <= new Date('2023-12-31')) {
@@ -141,7 +141,7 @@ module.exports = {
             (err_gift, results_gift, fields) => {
               if (err) {
                 console.log('쿠폰 요청:' + err_gift);
-                res.status(400).send({ msg: 'error', content: err });
+                res.status(400).send({msg: 'error', content: err});
               } else {
                 console.log(
                   '쿠폰 지급 성공 결과값:' + JSON.stringify(results_gift),
