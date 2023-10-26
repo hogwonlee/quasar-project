4a023936ae9276aaab9321a44bc3d20bf213191e
hogwon <hokwonlee88@gmail.com>
2023-10-26 23:39:14 +0900

    test

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -332,7 +332,7 @@
           var dis = document.querySelector('.' + c.category).offsetTop - 750;
           if (dis > 0 && dis < closest_dis) {
             closest_dis = dis;
-            closest_category = c;
+            closest_category = c.category;
           }
         });
         this.handleScroll(closest_category);
