d5f347a02275d6ef452ed102cf0b26fd3df7e321
hogwon <hokwonlee88@gmail.com>
2023-11-03 12:10:04 +0900

    냉동식품 배송비 추가 / 배송비 3000으로 변경

--- src/store/cartList.js
+++ src/store/cartList.js
@@ -56,9 +56,14 @@ const getters = {
   },
   freeze_shipmentPrice: (state, getters) => {
     var freeze_product;
-    freeze_product = state.items.find(product =>( product.category == '冷藏l냉장보관' || product.category == '速冻l냉동보관' ))
-    console.log('냉동식품'+freeze_product)
-    return  getters.cartTotalPrice >= 30000 || getters.cartTotalPrice === 0
+    freeze_product = state.items.find(
+      product =>
+        product.category == '冷藏l냉장보관' ||
+        product.category == '速冻l냉동보관',
+    );
+    return freeze_product == undefined
+      ? 0
+      : getters.cartTotalPrice >= 50000
       ? 0
       : 3000;
   },
