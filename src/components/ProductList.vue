51695692c6df998d3960c21336a41d7a2d7dd203
hogwon <hokwonlee88@gmail.com>
2023-11-03 12:41:28 +0900

    버튼 상위레이어로 이동 / 냉동 안내 텍스트

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -22,50 +22,7 @@
         <q-icon name="search" color="dark" />
       </template>
     </q-input>
-    <q-page-sticky
-      v-if="products.length > 0"
-      position="bottom-right"
-      :offset="[10, 10]"
-    >
-      <q-fab
-        v-model="list_show"
-        persistent="false"
-        :label="selected_local.category"
-        direction="up"
-        class="q-pa-none q-gutter-none"
-        color="dark"
-        vertical-actions-align="right"
-        icon="keyboard_arrow_up"
-      >
-        <q-fab-action
-          :label="c.category"
-          padding="3px"
-          v-for="c in category.slice().sort().reverse()"
-          :key="c.category"
-          v-bind="c"
-          color="dark"
-          @click="handleScroll(c.category)"
-        />
-      </q-fab>
-    </q-page-sticky>
-    <q-page-sticky position="bottom-left" :offset="[10, 50]">
-      <q-btn
-        :label="selected_local.prev_category"
-        icon="keyboard_arrow_up"
-        class="bg-dark text-white"
-        rounded
-        @click="go_prev_category()"
-      ></q-btn>
-    </q-page-sticky>
-    <q-page-sticky position="bottom-left" :offset="[10, 10]">
-      <q-btn
-        :label="selected_local.next_category"
-        icon="keyboard_arrow_down"
-        class="bg-dark text-white"
-        rounded
-        @click="go_next_category()"
-      ></q-btn>
-    </q-page-sticky>
+
     <div v-if="products.length > 0">
       <div
         v-show="showSimulatedReturnData"
@@ -86,11 +43,13 @@
             <div>
               {{ selected_local.freeze_deliver_info1 }}
             </div>
+            <q-space />
           </q-bar>
           <q-bar dense>
             <div>
               {{ selected_local.freeze_deliver_info2 }}
             </div>
+            <q-space />
           </q-bar>
         </div>
         <KeepAlive>
@@ -114,6 +73,50 @@
             "
           />
         </KeepAlive>
+        <q-page-sticky
+          v-if="products.length > 0"
+          position="bottom-right"
+          :offset="[10, 10]"
+        >
+          <q-fab
+            v-model="list_show"
+            persistent="false"
+            :label="selected_local.category"
+            direction="up"
+            class="q-pa-none q-gutter-none"
+            color="dark"
+            vertical-actions-align="right"
+            icon="keyboard_arrow_up"
+          >
+            <q-fab-action
+              :label="c.category"
+              padding="3px"
+              v-for="c in category.slice().sort().reverse()"
+              :key="c.category"
+              v-bind="c"
+              color="dark"
+              @click="handleScroll(c.category)"
+            />
+          </q-fab>
+        </q-page-sticky>
+        <q-page-sticky position="bottom-left" :offset="[10, 50]">
+          <q-btn
+            :label="selected_local.prev_category"
+            icon="keyboard_arrow_up"
+            class="bg-dark text-white"
+            rounded
+            @click="go_prev_category()"
+          ></q-btn>
+        </q-page-sticky>
+        <q-page-sticky position="bottom-left" :offset="[10, 10]">
+          <q-btn
+            :label="selected_local.next_category"
+            icon="keyboard_arrow_down"
+            class="bg-dark text-white"
+            rounded
+            @click="go_next_category()"
+          ></q-btn>
+        </q-page-sticky>
       </div>
     </div>
     <div v-if="!showSimulatedReturnData || !(products.length > 0)">
