31ec706575f8c21e1225630e8bb3bad6e2dface1
hogwon <hokwonlee88@gmail.com>
2023-11-09 23:20:34 +0900

    버튼 위치 변경

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -301,7 +301,7 @@
             />
           </q-card-section>
         </q-card>
-        <q-page-sticky class="z-top" position="bottom" :offset="[0, 10]">
+        <q-page-sticky class="z-top" position="bottom-right" :offset="[10, 10]">
           <q-btn
             class="z-top"
             stack
