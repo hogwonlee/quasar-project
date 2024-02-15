b0eeae896f31aae6dcf054029884f10a3e3a273a
hogwon <hokwonlee88@gmail.com>
2024-02-15 11:08:58 +0900

    test

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -63,10 +63,7 @@
     </div>
     <div>
       <q-dialog v-model="card" :id="category" class="q-px-none q-mx-none">
-        <div
-          class="q-pa-md bg-white"
-          style="height: min-content; width: min-content"
-        >
+        <div class="bg-white">
           <q-carousel
             v-model="slide"
             transition-prev="slide-right"
