4699b5f4643fce8bd8e52d355b0c21709b36902b
hogwon <hokwonlee88@gmail.com>
2024-02-24 21:02:37 +0900

    test

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -207,7 +207,7 @@
                   <q-icon name="shopping_cart" /> </template
               ></q-input>
               <q-btn
-                :disable="localQuantity <= 10"
+                :disable="localQuantity <= 9"
                 class="col-2"
                 label="-10"
                 text-color="negative"
