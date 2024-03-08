d8693c98697e9f1920a6ec0118817619cc09325a
hogwon <hokwonlee88@gmail.com>
2024-03-08 21:16:49 +0900

    test

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -121,7 +121,11 @@
             :model-value="product_name_ko"
           /> -->
           <img :src="img" style="margin-top: 3px; margin-bottom: 3px" />
-          <div>---底线---</div>
+          <div style="text-align: center">---底线---</div>
+          <q-card-section
+            class="transparent"
+            style="height: 30px"
+          ></q-card-section>
         </q-card>
         <q-page-sticky class="bg-white" position="bottom" :offset="[0, -60]">
           <q-card-section class="row q-mt-none q-py-none q-px-sm">
