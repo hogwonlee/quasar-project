245d625d6d7c198e4ddfa32d884145bf52b72078
hogwon <hokwonlee88@gmail.com>
2024-05-23 15:19:23 +0900

    googlelogin 리다이렉트 페이지

---
+++ src/components/auth/GoogleLogin.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>구글 로그인 인증 리다이렉트</h3>
+  </div>
+</template>
+
+<script>
+  import {defineComponent} from 'vue';
+
+  export default defineComponent({
+    name: 'GoogleLogin.vue',
+  });
+</script>
