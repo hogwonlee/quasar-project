7202e459cd1dbe113e956756aaa4b89a182b58de
hogwon <hokwonlee88@gmail.com>
2025-02-07 19:48:26 +0900

    서버 주소 지정 제거

--- src/configs/index.js
+++ src/configs/index.js
@@ -6,7 +6,7 @@ process.env.VUE_APP_TOSS_SECRET_KEY = jsonFile.VUE_APP_TOSS_SECRET_KEY;
 process.env.VUE_APP_BRANDPAY_CLIENT_KEY = jsonFile.VUE_APP_BRANDPAY_CLIENT_KEY;
 process.env.VUE_APP_BRANDPAY_SECRET_KEY = jsonFile.VUE_APP_BRANDPAY_SECRET_KEY;
 export default {
-  server: process.env.VUE_APP_BACKEND_SERVER || 'http://175.119.224.213:3000', //'http://182.215.249.216:3000', // 'http://localhost:3000',
+  server: process.env.VUE_APP_BACKEND_SERVER, // || 'http://175.119.224.213:3000', //'http://182.215.249.216:3000', // 'http://localhost:3000',
   clientKey: process.env.VUE_APP_TOSS_CLIENT_KEY,
   secretKey: process.env.VUE_APP_TOSS_SECRET_KEY,
   brandpayClientKey: process.env.VUE_APP_BRANDPAY_CLIENT_KEY,
