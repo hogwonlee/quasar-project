69742112cc6a428a2d6792db5b2fce90e77a2d76
hogwon <hokwonlee88@gmail.com>
2025-03-11 10:36:54 +0900

    127 -> localhost

--- quasar.config.js
+++ quasar.config.js
@@ -105,8 +105,8 @@ module.exports = configure(function (/* ctx */) {
       proxy: {
         // proxy all requests starting with /api to jsonplaceholder
         '/api': {
-          target: 'http://127.0.0.0:3000/',
-          changeOrigin: true,
+          target: 'http://localhost:3000/',
+          // changeOrigin: true,
           pathRewrite: {
             '^/api': '',
           },
