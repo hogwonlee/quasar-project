3d0fa2437d8cf54de744b4469d34cd914a784d9f
hogwon <hokwonlee88@gmail.com>
2025-03-15 14:33:35 +0900

    log 추가

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -132,7 +132,7 @@ module.exports = {
   },
 
   login: async (req, res) => {
-    // console.log('로그인 함수가 실행됩니다.');
+    console.log('로그인 요청: ' + JSON.stringify(req));
     const sqlCommend =
       'SELECT * FROM userinfo LEFT OUTER JOIN addressinfo ON userinfo.id = addressinfo.user_id WHERE userinfo.id = ? AND userinfo.user_pw = ? ';
     const body = req.body;
