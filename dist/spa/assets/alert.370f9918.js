fb583a8464424bfc999d4800c70a379b4e40952b
hogwon <hokwonlee88@gmail.com>
2025-05-10 15:05:29 +0900

    build 수정. index.json 수정

--- dist/spa/assets/alert.370f9918.js
+++
@@ -1,1 +0,0 @@
-import{aN as s}from"./index.ccc5e964.js";function t(a,e){s.create({title:a,message:e,persistent:!1}).onOk(o=>{}).onCancel(()=>{}).onDismiss(()=>{})}var n={confirm:t};export{n as a};
--- src/configs/index_local_test.js
+++
@@ -1,6 +0,0 @@
-export default {
-  server: process.env.VUE_APP_BACKEND_SERVER || 'http://175.119.224.213:3000', //'http://182.215.249.216:3000', // 'http://localhost:3000',
-
-  clientKey: 'test_ck_Lex6BJGQOVD5xn945RarW4w2zNbg',
-  secretKey: 'dGVzdF9za19PNkJZcTdHV1BWdk12a0piTHB3M05FNXZibzFkOg==',
-};
