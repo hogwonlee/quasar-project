b9918c34db351e056cba678475a33f438804a4d7
hogwon <hokwonlee88@gmail.com>
2026-01-13 09:14:14 +0900

    아이폰 첫 로딩 시 상품이 처음부터 노출되도록 수정

---
+++ dist/spa/assets/QSpace.f8947fd3.js
@@ -0,0 +1,1 @@
+import{h as e,c as a}from"./index.648bb071.js";const c=e("div",{class:"q-space"});var s=a({name:"QSpace",setup(){return()=>c}});export{s as Q};
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -318,10 +318,11 @@
           done();
           return;
         }
-        index = this.showing_products.length / 24;
-        const startIndex = index * 24;
-        const endIndex = startIndex + 24;
-        const newProducts = this.products.slice(startIndex, endIndex);
+
+        // const startIndex = (index - 1) * 24;
+        const startIndex = this.showing_products.length;
+        // const endIndex = startIndex + 24;
+        const newProducts = this.products.slice(startIndex, startIndex + 24);
         // 만약 더 이상 불러올 데이터가 없으면 무한 스크롤을 중지
         if (newProducts.length === 0) {
           this.hasMore = false;
